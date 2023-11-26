7d886bfcd32f6a0f01c3aaf8ff19d5c18246180b
KolwaBrad <duncan.bradley@strathmore.edu>
2023-11-26 09:51:03 +0300

    postioning adjustments

--- trial.py
+++ trial.py
@@ -1,57 +1,61 @@
 import pygame
 import sys
 import random
-import time  # Needed for timing
+import time
 
 pygame.init()
 
-width, height = 1920, 1080  # Updated resolution
+width, height = 1920, 1080
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 bird_radius = 25
-bird = pygame.Rect(width // 2 - bird_radius, height // 2 - bird_radius, bird_radius * 2, bird_radius * 2)
+bird_y_start = height // 2
+bird = pygame.Rect(width // 2 - bird_radius, bird_y_start - bird_radius, bird_radius * 2, bird_radius * 2)
 gravity = 0.5
 jump = -10
 velocity = 0
 
 floor_color = (34, 177, 76)
-grass_height = 50
+grass_height = int(height * 0.5)  # Raise the floor position by 50%
 
 obstacle_width = 50
-obstacle_height = 150  # Adjusted height
+obstacle_height = int(150 * 0.25)  # Reduce the height by 75%
 obstacle_color = (255, 0, 0)
 obstacle_x = width
-obstacle_y = height - obstacle_height
+obstacle_y = height - obstacle_height - grass_height
+obstacle_min_height = int(100 * 0.25)  # Adjusted minimum height
+obstacle_max_height = int(300 * 0.25)  # Adjusted maximum height
 
-start_time = time.time()  # Start timer
+start_time = time.time()
 
 def reset_obstacle():
     global obstacle_x, obstacle_y, obstacle_height
     obstacle_x = width
-    obstacle_height = random.randint(100, 300)  # Adjust height range for randomness
-    obstacle_y = height - obstacle_height
+    obstacle_height = random.randint(obstacle_min_height, obstacle_max_height)
+    obstacle_y = height - obstacle_height - grass_height
 
 def pause_game():
     pause = True
+    font = pygame.font.SysFont(None, 55)
+    text = font.render('Game Paused: R to Restart, Q to Quit', True, (0, 0, 0))
+    text_rect = text.get_rect(center=(width // 2, height // 2))
+
     while pause:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:  # Press 'R' to restart
+                if event.key == pygame.K_r:
                     reset_obstacle()
                     return True
-                elif event.key == pygame.K_q:  # Press 'Q' to quit
+                elif event.key == pygame.K_q:
                     return False
 
         screen.fill((135, 206, 250))
-        font = pygame.font.SysFont(None, 55)
-        text = font.render('Game Paused: R to Restart, Q to Quit', True, (0, 0, 0))
-        screen.blit(text, (20, height // 2 - 20))
+        screen.blit(text, text_rect)
         pygame.display.flip()
-
         clock.tick(60)
 
 while True:
@@ -66,22 +70,20 @@ while True:
 
     velocity += gravity
     bird.y += velocity
-    bird.y = max(0, min(bird.y, height - bird.height))
+    bird.y = max(0, min(bird.y, height - bird.height - grass_height))
 
-    if time.time() - start_time > 3:  # Wait for 3 seconds before moving the obstacle
+    if time.time() - start_time > 3:
         obstacle_x -= 5
-
         if obstacle_x + obstacle_width < 0:
             reset_obstacle()
 
     obstacle_rect = pygame.Rect(obstacle_x, obstacle_y, obstacle_width, obstacle_height)
     if bird.colliderect(obstacle_rect):
-        if not pause_game():  # If the player chooses to quit
+        if not pause_game():
             pygame.quit()
             sys.exit()
 
     screen.fill((135, 206, 250))
-
     pygame.draw.rect(screen, floor_color, (0, height - grass_height, width, grass_height))
     pygame.draw.circle(screen, (255, 0, 0), bird.center, bird_radius)
     pygame.draw.rect(screen, obstacle_color, obstacle_rect)
